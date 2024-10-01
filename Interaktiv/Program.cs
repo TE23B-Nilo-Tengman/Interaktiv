9b1689feb7d5336151fe69986213a7cd65db8563
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2024-10-01 12:58:08 +0200

    interaktiv

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("är du man eller kvinna?");
+
+
+string kön = Console.ReadLine();
+if (kön.ToLower() == "man")
+{
+    Console.WriteLine("ok, mjukisar eller jeans?");
+}
+else if (kön.ToLower() == "kvinna")
+    {
+        Console.WriteLine("ok, tjol eller klänning?");
+    }
+
+
+string byx = Console.ReadLine();
+if (byx.ToLower() == "mjukisar")
+{
+    Console.WriteLine("k, awsome jag håller med!");
+}
+else if (byx.ToLower() == "jeans")
+{
+    Console.WriteLine("brosan tror han e skibidi sigma elle nåt");
+}
+
+
+string tjo = Console.ReadLine();
+if (tjo.ToLower() == "tjol")
+{
+    Console.WriteLine("kys");
+}
+else if (tjo.ToLower() == "klänning")
+{
+    Console.WriteLine("kys");
+}
+
+
+
+
+
+
+Console.ReadLine();
--- Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
+++ Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e198cc23f05c66acfb3f5565e460df8114383cad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
